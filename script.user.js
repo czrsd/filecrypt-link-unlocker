22746579af9b3dfbe0e2591e9131144adae1b7fd
Marco <marco077x@gmail.com>
2025-09-07 16:56:14 +0200

    Add license

--- script.user.js
+++ script.user.js
@@ -8,6 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=filecrypt.cc
 // @grant        none
 // @run-at       document-start
+// @license      MIT
 // ==/UserScript==
 
 (() => {
